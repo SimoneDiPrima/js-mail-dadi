3900c5367c1418a442a1c97eed7d08758806253c
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-24 15:10:19 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*
+Crea (inventa) una lista di inidirizzi mail autorizzati.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa in console un messaggio appropriato sull’esito del controllo.
+ ("Benvenuto" se la mail è nella lista, "Spiacenti" se non lo è);
+ */
+
+//  1-creare un array con il nome delle mail autorizzate e inserirlo in una variabile array
+// 2-chiedo una mail all utente(forse meglio come input che come prompt);
+// 3-verifico se l email è autorizzata o no e quindi se fa parte della lista delle mail autorizzate o no
+
+
+const mailAutorizzate = ["diprima.simone@virgilio.it",
+                        "mario.rossi@virgilio.it","gerry.Scotti@virgilio.it","paoloverdi@virgilio.it",
+                        "mariobanchi@virgilio.it"];
+                        
