8d19f0da8108d7a0a9775218ba04faa0f6034e61
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-24 17:38:04 +0200

    impostazione ciclo dadi

--- js/scriptdadi.js
+++ js/scriptdadi.js
@@ -1,1 +1,23 @@
-console.log(`JS OK`);
+
+/*Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilisci il vincitore, in base a chi fa il punteggio più alto e stampa 
+in console il risultato (ossia se ha vinto il computer o il player)
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?*/
+
+// 1-una variabile per il giocatore e una variabile per il computer;
+/*2-crearle con il let almeno poi ci metto la funzione Math.random(ricordarsi che voglio i valori da 1 a 6);
+-3 stabilisci il vincitore in base a chi fa il punteggio piu alto e stampa a console the winner!*/
+ //o mettere la funzione math random dentro a dado e i due giocatori come costanti singole?
+
+ const dado = [0,1,2,3,4,5,6];
+ 
+ 
+
+
+
+
+ for(i=0;i<dado.length;i++){
+    console.log(dado[i]);
+ }
